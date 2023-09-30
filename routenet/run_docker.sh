52a24532cd8085118cc31ab59716bf16bdb56eda
Guillem Bonet <guillem.bonet@gmail.com>
2023-09-30 14:42:18 +0200

    add docker command to leave container in background

--- routenet/run_docker.sh
+++ routenet/run_docker.sh
@@ -1,1 +1,3 @@
 docker run -it --rm --name routenet -v $(pwd):/home tensorflow/tensorflow:2.6.1 sh -c "pip install networkx; cd home; bash"
+
+#docker run -d --name routenet -v $(pwd):/home tensorflow/tensorflow:2.6.1 sh -c "pip install networkx; sleep infinity"
